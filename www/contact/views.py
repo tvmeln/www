64dafeccc95bc88314d3fe05fde15aba7c7556fe
Timofey <japidrawq@inbox.ru>
2024-05-01 18:29:32 +0300

    urls ready

--- www/contact/views.py
+++ www/contact/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'contact/contact.html')
--- www/login/views.py
+++ www/login/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login/login.html')
 # Create your views here.
